b8b7173ca7ef7c5726bb1a47cb130276792ec628
andy-maca <66229754@qq.com>
2018-04-23 10:00:35 +0800

    use java print

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("##teamcity[compilationStarted compiler='compilejava']");
-        System.out.println("##teamcity[message text='compiler error' status='ERROR']");
-        System.out.println("##teamcity[compilationFinished compiler='compilejava']");
-
-        System.err.println("##teamcity[compilationStarted compiler='compilejava']");
-        System.err.println("##teamcity[message text='compiler error' status='ERROR']");
-        System.err.println("##teamcity[compilationFinished compiler='compilejava']");
-
-
-        System.err.print("This is error message print to System.err");
-        System.exit(1);
+        System.out.println(args[0]);
     }
 }
